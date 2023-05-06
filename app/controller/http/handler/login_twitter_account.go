4817a1d3cc284ba1acf038dd9c0b2486dbcf595f
mahiro72 <mahiro.yoshie@gmail.com>
2023-05-06 18:58:15 +0900

    add: empty handler

---
+++ app/controller/http/handler/login_twitter_account.go
@@ -0,0 +1,12 @@
+package handler
+
+import "net/http"
+
+type LoginTwitterAccountRequest struct {
+
+}
+
+func LoginTwitterAccountRequestHandler() func(w http.ResponseWriter,r *http.Request) {
+
+}
+
